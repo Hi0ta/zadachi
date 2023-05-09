90d22b3babd7bccab840554869248675f4362b70
Ann <niyta@mail.ru>
2023-05-09 20:57:34 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    // 11 Дан массив строк. Необходимо удалить из него все дубли, а оставшиеся строки объединить в одну в порядке следования в массиве.
+
+        String[] arrString = {"строка1", "строка5", "строка1", "строка2", "строка2", "строка4", "строка1", "строка3", "строка6", "строка1"};
+        String itog = Arrays.stream(arrString).distinct().collect(Collectors.joining());
+        System.out.println(itog);
+
+    }
+}
